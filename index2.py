33e44c4f506c3c11fa1684d4085bacc94f7dee0b
Leonardo <leonardoemaill1@gmail.com>
2024-10-05 05:44:06 -0300

    ver 9

---
+++ index2.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from routes.home import home_route
+from routes.cliente import cliente_route
+from routes.login import login_route
+from routes.pesquisa import pesquisa_route
+from routes.usercad import usercad_route
+from routes.professioncad import professioncad_route
+
+app = Flask(__name__, template_folder="/templates")
+
+app.secret_key = 'mack_enzie_key'
+
+@app.route('/year')
+def home():
+    return 'Hello, World!'
+
+
+app.register_blueprint(login_route)
+app.register_blueprint(home_route, url_prefix='/home')
+app.register_blueprint(cliente_route, url_prefix='/clientes')
+app.register_blueprint(pesquisa_route, url_prefix='/pesquisa')
+app.register_blueprint(usercad_route, url_prefix='/user')
+app.register_blueprint(professioncad_route, url_prefix='/professioncad')
