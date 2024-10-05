32327b522f43e1ef219ad5ca51dde7c1e41e8449
Leonardo <leonardoemaill1@gmail.com>
2024-10-05 01:28:47 -0300

    Terceiro commit

--- routes/cadastro.py
+++ routes/cadastro.py
@@ -4,5 +4,5 @@ home_route = Blueprint('cadastro',__name__)
 
 @home_route.route('/cadastro')
 def home():
-    return render_template('indexL.html')
+    return render_template('indexL.html') #html de destino
 
