71e8558c9c18223fe4025e2822f2f53c286695b2
leo <leo@gmail.com>
2024-11-21 20:33:48 -0300

    alteracao_x

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from routes.webhook import webhook_route
 from routes.atualizarPerfil import atualizarPerfil_route
 
 
-# inicialização (Sempre no início) jjj
+# inicialização (Sempre no início) xxxxxxxx
 app = Flask(__name__)
 
 app.secret_key = 'mack_enzie_key'
